80f077b66eea0fef165b14a41b57a21014ab8685
TranMK <tran2ki@mail.uc.edu>
2025-02-24 15:37:27 -0500

    Queue complete

--- Queue.cpp
+++ Queue.cpp
@@ -1,3 +1,37 @@
 #include <Queue.hpp>
 #include <Errors.hpp>
-
+template <class T> auto Queue<T>::length()->size_t {return length;}
+template <class T> auto Queue<T>::isfull()->bool {
+    return length == MAX_QUEUE_SIZE;
+}
+template <class T> auto Queue<T>::isempty() -> bool {
+    return length == 0;
+}
+template <class T> auto Queue<T>::enqueue(T *to_add) -> void {
+    if (isfull()) {
+      throw StackOverflowError{};
+    }
+    data[front] = to_add;
+    front = (front + 1) % MAX_QUEUE_SIZE;
+    length++;
+}
+template <class T> auto Queue<T>::dequeue() -> T * {
+    // Should do `data.empty()` instead we're using `std::array`.
+    if (isempty()) {
+      throw StackUnderflowError{};
+    }
+    length--;
+    size_t retval = data[back];
+    back = (back + 1) % MAX_QUEUE_SIZE;
+    return retval;
+}
+template <class T> auto Queue<T>::peek() -> T * {
+    return data[back]
+}
+template <class T> auto Queue<T>::empty() -> void {
+    for (auto &index : data){
+        index = nullptr;
+    }
+    length=0;
+}
+template <> class Queue<std::string> {};
--- Queue.hpp
+++ Queue.hpp
@@ -1,16 +1,23 @@
 #include <array>
-#pragma once
-template <class Q> class Queue{
-    private:
-        std::array<T *, MAX_STACK_SIZE> data;
-        size_t size;
 
+#pragma once
+
+const size_t MAX_QUEUE_SIZE = 10;
+
+template <class T> class Queue{
+    private:
+        std::array<T *, MAX_QUEUE_SIZE> data;
+        size_t length;
+        size_t front;
+        size_t back;
     public:
-        Stack(size_t size): size(size) {}
-        auto Enqueue(T *to_add)->void;
-        auto Dequeue()->T*;
-        auto Peek()->T*;
+        Queue(size_t length): length(length) {}
+        auto enqueue(T *to_add)->void;
+        auto dequeue()->T*;
+        auto peek()->T*;
         auto length()->size_t;
         auto empty()->void;
+        auto isempty()->bool;
+        auto isfull()->bool;
     
-}
+};
