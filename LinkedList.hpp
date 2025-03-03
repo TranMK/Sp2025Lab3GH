6fbd5de99fadc5dcaba498c66c0a165494fe59dd
castroploiin <fieryfire1005@gmail.com>
2025-03-03 15:09:22 -0500

    [refactor] change where data is stored

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -6,8 +6,30 @@
 
 template <class T> class DoublyLinkedList {
 private:
-  LinkedListNode<T> head;
-  size_t size;
+  LinkedListNode<T> *head;
+  size_t size = 0;
 
 public:
+  DoublyLinkedList(T init_value) : head(LinkedListNode(init_value)), size(1) {}
+  ~DoublyLinkedList() {
+    size = 0;
+
+    T *curr_node = head;
+    while (curr_node != nullptr) {
+      delete (curr_node);
+      curr_node = curr_node->next;
+    }
+  }
+
+  auto add_item() -> void;
+  auto get_item() -> LinkedListNode<T> *;
+
+  auto is_in_list() -> bool;
+  auto is_empty() -> bool;
+  auto get_size() -> size_t;
+
+  auto see_next() -> LinkedListNode<T>;
+  auto see_prev() -> LinkedListNode<T>;
+  auto see_at() -> LinkedListNode<T>;
+  auto reset() -> void;
 };
--- LinkedListNode.hpp
+++ LinkedListNode.hpp
@@ -1,20 +1,13 @@
 template <class T> class LinkedListNode {
 private:
-  T value;
+  T *value;
   LinkedListNode *next;
   LinkedListNode *prev;
 
 public:
-  LinkedListNode(T init_value) : value(init_value) {}
+  LinkedListNode(T *init_value) : value(init_value) {}
 
   auto get_value() -> LinkedListNode * { return value; }
   auto get_next() -> LinkedListNode * { return next; }
   auto get_prev() -> LinkedListNode * { return prev; }
-
-  auto add_item() -> void;
-  auto get_item() -> void;
-
-  auto is_in_list() -> void;
-  auto is_empty() -> void;
-  auto size() -> void;
 };
