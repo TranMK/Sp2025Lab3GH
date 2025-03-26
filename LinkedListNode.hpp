e4608a643e1475e4d830301e2cb143cd04c1f709
anshmendiratta <anshmendiratta2023@gmail.com>
2025-03-25 22:48:03 -0400

    .

--- LinkedListNode.hpp
+++ LinkedListNode.hpp
@@ -2,7 +2,7 @@
 
 template <class T> class LinkedListNode {
 private:
-  T value;
+  T *value;
   LinkedListNode *next;
   LinkedListNode *prev;
 
@@ -10,11 +10,11 @@ public:
   LinkedListNode() = default;
   LinkedListNode(T init_value) : value(init_value) {}
 
-  auto get_value() -> T { return value; }
+  auto get_value() -> T* { return value; }
   auto get_next() -> LinkedListNode * { return next; }
   auto get_prev() -> LinkedListNode * { return prev; }
 
-  auto set_value(T set_value) -> void { value = set_value; }
+  auto set_value(T *set_value) -> void { value = set_value; }
   auto set_next(LinkedListNode *next_node) -> void { next = next_node; }
   auto set_prev(LinkedListNode *prev_node) -> void { prev = prev_node; }
 };
