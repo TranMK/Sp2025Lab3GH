44b827b106c5c24aa27f7aeaa338b306b478691c
castroploiin <fieryfire1005@gmail.com>
2025-04-21 16:04:11 -0400

    .

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@
 #include "linked_list.hpp"
 
 void bubble_sort(size_t arr);
-template <size_t SIZE, class T> auto test(std::array<T, SIZE> &) -> double;
+template <size_t SIZE, class T> auto benchmark(std::array<T, SIZE> &) -> double;
 
 auto main() -> int { return 0; }
 
-template <size_t SIZE, class T> auto test(std::array<T, SIZE> &arr) -> double {
+template <size_t SIZE, class T>
+auto benchmark(std::array<T, SIZE> &arr) -> double {
   return 0;
 }
